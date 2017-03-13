ebd1e1daeb9ca535202d2e7360ed0b8490f829bf
jw <chlwodnjs34@naver.com>
2017-03-13 16:17:56 +0900

    new message bot-5 test5

--- app.js
+++ app.js
@@ -833,8 +833,8 @@ var url2;
 var value = new Array();
 var check = new Array();
 
-function abbc() {
-request(url, function(error, response, body) {  
+function parsing() {
+  request(url, function(error, response, body) {  
     if (error) throw error;
 
     var $ = cheerio.load(body);
@@ -876,6 +876,18 @@ request(url, function(error, response, body) {
     }); //each
   }); //request
 }
+function uploadWebtoon(){
+  for (var i = 0; i < value.length; i++) {
+    if(check[i] != true && check[i] == false){
+      console.log(value[i][0]);
+      /*console.log(value[i][0] + " " + value[i][1] + " 업로드 되었습니다." + value[i][2]);*/
+      check[i] = true;
+    }else{
+      
+    }
+  }
+}
+
 // Start server
 // Webhooks must be available via SSL with a certificate signed by a valid 
 // certificate authority.
