03d94d55507a58bf5c3e49efc277b171bd7d9b96
jw <chlwodnjs34@naver.com>
2017-03-13 14:45:23 +0900

    new message bot-7

--- app.js
+++ app.js
@@ -18,7 +18,6 @@ const
   https = require('https'),  
   request = require('request');
 
-var cheerio = require("cheerio");
 var app = express();
 app.set('port', process.env.PORT || 5000);
 app.set('view engine', 'ejs');
@@ -307,9 +306,7 @@ function receivedMessage(event) {
       case 'account linking':
         sendAccountLinking(senderID);
         break;
-      case '알림해줘':
-        startAlert(senderID);
-        break;
+
       default:
         sendTextMessage(senderID, messageText);
     }
@@ -533,23 +530,6 @@ function sendTextMessage(recipientId, messageText) {
   };
 
   callSendAPI(messageData);
-}
-
-function testFun(recipientId, webtoon){
-  var messageData = {
-    recipient:{
-      id:recipientId
-    },
-    message: {
-      text: webtoon
-    }
-  };
-
-  callSendAPI(messageData);
-}
-
-function startAlert(recipientId){
-  adminId = recipientId;
 }
 
 /*
@@ -846,84 +826,6 @@ function callSendAPI(messageData) {
     }
   });  
 }
-
-
-
-
-/*var url = "http://comic.naver.com/webtoon/weekday.nhn";
-var url2;
-var value = new Array();
-var check = new Array();
-var adminId = "";
-
-play = setInterval(function(){
-  request(url, function(error, response, body) {  
-    if (error) throw error;
-
-    var $ = cheerio.load(body);
-
-    var postWeek = $("div.col li");
-
-    postWeek.each(function(index) {
-      var postTitle = $(this).find(".title").text();
-      var postLink = $(this).find("a").attr("href");
-      value[index] = new Array();
-      value[index][0] = postTitle;
-      
-      var url2 = "http://comic.naver.com" + postLink;
-      
-      if($(this).find('.ico_updt').length>=1  && check[index] != true){
-        
-        request(url2, function(error, response, body) {  
-        if (error) throw error;
-
-        var $ = cheerio.load(body);
-
-        var postWeek = $("tr td.title").eq(0);
-        
-        postWeek.each(function(){
-          var a = $(this).find("a").text();
-
-          if($("tr td").eq(0).find("a").attr("href")=="#"){//첫 td가 미리보기일 경우
-            var b = $("tr td").eq(1).find("a").attr("href");
-          }else{
-            var b = $("tr td").eq(0).find("a").attr("href");
-          }
-          
-          value[index][1] = a;
-          value[index][2] = "http://comic.naver.com" + b;
-          check[index] = false;
-          uploadWebtoon();
-        }); //each
-      }); //request
-      }
-    }); //each
-  }); //request
-
-},60*1000);
-
-function uploadWebtoon(){
-  
-  for (var i = 0; i < value.length; i++) {
-    if(check[i] != true && check[i] == false){
-      if(adminId){
-        console.log(value[i][0]);
-        /*console.log(value[i][0] + " " + value[i][1] + " 업로드 되었습니다." + value[i][2]);*/
-        check[i] = true;
-        var webtoon = value[i][0] + " " + value[i][1] + " 업로드 되었습니다." + value[i][2];
-        
-        testFun(adminId, webtoon);
-      }
-    }else{
-      
-    }
-  }
-}*/
-
-
-
-
-
 
 // Start server
 // Webhooks must be available via SSL with a certificate signed by a valid 
