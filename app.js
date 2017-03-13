bef111d92c3bce505dbdc91429de8874d4b09e2d
jw <chlwodnjs34@naver.com>
2017-03-13 14:58:10 +0900

    new message bot-5 test3

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.json({ verify: verifyRequestSignature }));
 app.use(express.static('public'));
 
+var userId ;
 /*
  * Be sure to setup your config values before running this code. You can 
  * set them using environment variables or modifying the config file in /config.
@@ -305,6 +306,10 @@ function receivedMessage(event) {
 
       case 'account linking':
         sendAccountLinking(senderID);
+        break;
+
+      case '알림해줘':
+        userId = senderID;
         break;
 
       default:
@@ -826,6 +831,94 @@ function callSendAPI(messageData) {
     }
   });  
 }
+
+
+
+var request = require("request");
+var cheerio = require("cheerio");
+
+var url = "http://comic.naver.com/webtoon/weekday.nhn";
+var url2;
+var value = new Array();
+var check = new Array();
+
+play = setInterval(function(){
+  request(url, function(error, response, body) {  
+    if (error) throw error;
+
+    var $ = cheerio.load(body);
+
+    var postWeek = $("div.col li");
+
+    postWeek.each(function(index) {
+      var postTitle = $(this).find(".title").text();
+      var postLink = $(this).find("a").attr("href");
+      value[index] = new Array();
+      value[index][0] = postTitle;
+      
+      var url2 = "http://comic.naver.com" + postLink;
+      
+      if($(this).find('.ico_updt').length>=1  && check[index] != true){
+        
+        request(url2, function(error, response, body) {  
+        if (error) throw error;
+
+        var $ = cheerio.load(body);
+
+        var postWeek = $("tr td.title").eq(0);
+        
+        postWeek.each(function(){
+          var a = $(this).find("a").text();
+
+          if($("tr td").eq(0).find("a").attr("href")=="#"){//첫 td가 미리보기일 경우
+            var b = $("tr td").eq(1).find("a").attr("href");
+          }else{
+            var b = $("tr td").eq(0).find("a").attr("href");
+          }
+          
+          value[index][1] = a;
+          value[index][2] = "http://comic.naver.com" + b;
+          check[index] = false;
+          
+        }); //each
+      }); //request
+      }
+    }); //each
+  }); //request
+
+},60*1000);
+
+function uploadWebtoon(){
+  for (var i = 0; i < value.length; i++) {
+    if(check[i] != true && check[i] == false){
+      console.log(value[i][0]);
+      /*console.log(value[i][0] + " " + value[i][1] + " 업로드 되었습니다." + value[i][2]);*/
+      check[i] = true;
+      var webtoon = value[i][0] + " " + value[i][1] + " 업로드 되었습니다." + value[i][2];
+      asd(userId, webtoon);
+    }else{
+      
+    }
+  }
+}
+
+function asd(recipientId, webtoon){
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      text: webtoon
+    }
+  };
+  callSendAPI(messageData);
+}
+
+
+
+
+
+
 
 // Start server
 // Webhooks must be available via SSL with a certificate signed by a valid 
